97c4104f94c760348708a7b66d3df911e21e217f
Wassim <68751630+Wassim-dev-wq@users.noreply.github.com>
2024-10-04 09:26:53 +0200

    MYD-5: Service discovery set up

---
+++ service-discovery/src/test/java/org/fivy/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fivy.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
