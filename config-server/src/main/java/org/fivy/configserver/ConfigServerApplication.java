72ecb8fb524f219fcb7cb6c4314e48b825dcdc60
Wassim <68751630+Wassim-dev-wq@users.noreply.github.com>
2024-10-22 12:17:06 +0200

    MYD-8 Cloud bus setup

--- config-server/src/main/java/org/fivy/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/org/fivy/configserver/ConfigServerApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,11 +11,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
-		dotenv.entries().forEach(e -> {
-			System.out.println("Loaded: " + e.getKey() + "=" + e.getValue());
-		});
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
