ab92900d6e89c164c9132b893874287c1e549883
Wassim <68751630+Wassim-dev-wq@users.noreply.github.com>
2025-04-01 21:56:35 +0200

    feat(gateway): [MYD-56] enhance security configuration and routing

--- api-gateway/src/main/java/org/fivy/apigateway/config/GatewayConfiguration.java
+++ api-gateway/src/main/java/org/fivy/apigateway/config/GatewayConfiguration.java
@@ -1,6 +1,7 @@
 package org.fivy.apigateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.GatewayFilterSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,14 +14,19 @@ public class GatewayConfiguration {
         return builder.routes()
                 .route("user-service", r -> r
                         .path("/api/v1/users/**")
+                        .filters(GatewayFilterSpec::tokenRelay)
                         .uri("lb://USER-SERVICE"))
+                .route("notification-service", r -> r
+                        .path("/api/v1/notifications/**", "/api/v1/user-push-tokens/**")
+                        .filters(GatewayFilterSpec::tokenRelay)
+                        .uri("lb://NOTIFICATION-SERVICE"))
                 .route("match-service", r -> r
-                        .path("/api/v1/matches/**", "/api/v1/notifications/**", "/api/v1/user-push-tokens/**")
+                        .path("/api/v1/matches/**","/api/v1/player/**","/api/v1/feedback/**")
+                        .filters(GatewayFilterSpec::tokenRelay)
                         .uri("lb://MATCH-SERVICE"))
                 .route("auth-service", r -> r
                         .path("/api/v1/auth/**")
                         .uri("lb://AUTH-SERVICE"))
                 .build();
     }
-
 }
