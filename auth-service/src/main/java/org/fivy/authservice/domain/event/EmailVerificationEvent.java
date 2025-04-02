b2fb2b96fc6405b6b4073e078221d05cd1408061
Wassim <68751630+Wassim-dev-wq@users.noreply.github.com>
2025-04-02 10:29:57 +0200

    feat(auth): [MYD-58] implement password reset and email verification

---
+++ auth-service/src/main/java/org/fivy/authservice/domain/event/EmailVerificationEvent.java
@@ -0,0 +1,16 @@
+package org.fivy.authservice.domain.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailVerificationEvent {
+    private String email;
+    private String verificationCode;
+    private int verificationCodeTtl;
+}
